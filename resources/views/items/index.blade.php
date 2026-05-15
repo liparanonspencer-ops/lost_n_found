3f3b2ca042245d5e5ce59e79e54a25f2363354c3
Crisjan0 <crisjancastro2@gmail.com>
2026-05-15 14:33:25 +0800

    fix picture folder

--- resources/views/items/index.blade.php
+++ resources/views/items/index.blade.php
@@ -38,7 +38,7 @@
                     
                     <div class="relative h-56 w-full overflow-hidden bg-slate-100 dark:bg-gray-700">
                         @if($item->image_path)
-                            <img src="/storage/{{ $item->image_path }}" class="w-full h-full object-cover group-hover:scale-110 transition-transform duration-500">
+                            <img src="{{ asset('storage/' . $item->image_path) }}" class="w-full h-full object-cover group-hover:scale-110 transition-transform duration-500">
                         @else
                             <div class="flex flex-col items-center justify-center h-full text-slate-400">
                                 <svg class="w-12 h-12 mb-2 opacity-20" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>
